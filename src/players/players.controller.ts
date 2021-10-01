f96b446ec98a30e9c0848aff6869216d8964edfe
vitoraa <vitor.lima@outlook.com>
2021-10-01 18:10:45 -0300

    get AllPlayers

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { Player } from './interfaces/player.interface';
 import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
@@ -11,7 +12,7 @@ export class PlayersController {
   }
 
   @Get()
-  async getAllPlayers () {
+  async getAllPlayers (): Promise<Player[]> {
     return await this.playersService.getAllPlayers();
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -9,15 +9,25 @@ export class PlayersService {
   private players: Player[] = [];
 
   async createUpdatePlayer (createPlayerDto: CreatePlayerDto): Promise<void> {
-    this.logger.log(`createPlayerDto: ${createPlayerDto}`);
-    this.create(createPlayerDto);
+    const { email } = createPlayerDto;
+    const playerFound = this.getPlayerByEmail(email);
+    if (playerFound) {
+      this.update(createPlayerDto, playerFound);
+    } else {
+      this.create(createPlayerDto);
+    }
   }
 
   async getAllPlayers (): Promise<Player[]> {
-    return this.players;
+    return await this.players;
+  }
+
+  private getPlayerByEmail (email: string): Player {
+    return this.players.find((player) => player.email === email);
   }
 
   private create (createPlayerDto: CreatePlayerDto): void {
+    this.logger.log(`createPlayerDto: ${createPlayerDto}`);
     const { name, email, phone } = createPlayerDto;
     const player: Player = {
       _id: uuidv4(),
@@ -29,5 +39,10 @@ export class PlayersService {
       urlPhoto: 'www.foto.com.br',
     };
     this.players.push(player);
+  }
+
+  private update (player: CreatePlayerDto, playerFound: Player): void {
+    const { name } = player;
+    playerFound.name = name;
   }
 }
